241d3b571192841b18aef4a26c90f46a53f79760
Nourhane <ramdaninourhane26@gmail.com>
2026-04-13 23:04:28 +0100

    update

--- core/blog/admin.py
+++ core/blog/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import BlogPost
 
-from .models import Food
-
-admin.site.register(Food)
+admin.site.register(BlogPost)
--- core/nutrition/models.py
+++ core/nutrition/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 
-# Create your models here.
-
 class Food(models.Model):
     name = models.CharField(max_length=100)
     calories = models.FloatField()
